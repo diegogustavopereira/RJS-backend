9e4917ad3e9922adfccbea98ba1134877520457e
Paula Bittencourt <paula.bittencourt@gmail.com>
2022-12-08 15:07:11 -0300

    route-post-login

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,10 +1,12 @@
 import express from "express";
 import bcrypt from "bcrypt";
 import UserModel from "../models/user.model.js";
+import generateToken from "../config/jwt.config.js";
 
 const router = express.Router();
 const rounds = 10;
 
+// CRIAR USUÁRIO
 router.post("/register", async (request, response) => {
     try {
         const { password } = request.body;
@@ -24,6 +26,31 @@ router.post("/register", async (request, response) => {
         delete user._doc.password; // protege a senha do usuário
 
         return response.status(201).json(user)
+
+    } catch (error) {
+        console.log(error);
+        return response.status(500).json({msg: "Ops... algo de errado não está certo"});
+    }
+})
+
+// FAZER LOGIN
+router.post("/login", async (request, response) => {
+    try {
+        const { email, password } = request.body;
+
+        const user = await UserModel.findOne({ email: email});
+
+        if(!user) {
+            return response.status(400).json({msg: "Erro nos dados de autenticação."});
+        }
+
+        if(await bcrypt.compare(password, user.password)) {
+            delete user._doc.password;
+            const token = generateToken(user);
+            return response.status(200).json({user: {...user._doc}, token: token});
+        } else {
+            return response.status(401).json({msg: "Erro nos dados de autenticação."});
+        }
 
     } catch (error) {
         console.log(error);
