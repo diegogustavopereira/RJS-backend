cbc7745a359054a65a17b8bc4641348924f61b7a
Paula Bittencourt <paula.bittencourt@gmail.com>
2022-12-12 08:52:46 -0300

    court-routes-get-id-put-delete

--- routes/courtInformations.routes.js
+++ routes/courtInformations.routes.js
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { request } from "express";
 import CourtInformationModel from "../models/courtInformations.model.js";
 
 const router = express.Router();
@@ -8,6 +8,23 @@ router.get("/", async (request, response) => {
     try {
         const courtInformation = await CourtInformationModel.find().populate("drugs");
         return response.status(200).json(courtInformation);
+
+    } catch (error) {
+        console.log(error);
+        return response.status(500).json({msg: "Ops... algo de errado não está certo"});
+    }
+})
+
+// GET BY ID
+router.get("/:id", async (request, response) => {
+    try {
+        const { id } = request.params;
+        const getCourtInformationById = await CourtInformationModel.findById(id);
+        
+        if(!getCourtInformationById) {
+            return response.status(404).json({msg: "Processo não encontrado."});
+        }
+        return response.status(200).json(getCourtInformationById);
 
     } catch (error) {
         console.log(error);
@@ -28,7 +45,33 @@ router.post("/create", async (request, response) => {
 })
 
 // PUT
+router.put("/edit/:id", async (request, response) => {
+    try {
+        const { id } = request.params;
+        const updateCourtInformation = await CourtInformationModel.findByIdAndUpdate(id, {...request.body}, {new: true, runValidators: true});
+        
+        if (!updateCourtInformation) {
+            return response.status(404).json({msg: "Processo não encontrado."});
+        }
+        return response.status(200).json(updateCourtInformation);
+
+    } catch (error) {
+        console.log(error);
+        return response.status(500).json({msg: "Ops... algo de errado não está certo"});
+    }
+})
 
 // DELETE
+router.delete("/delete/:id", async (request, response) => {
+    try {
+        const { id } = request.params;
+        const deleteCourtInformation = await CourtInformationModel.findByIdAndDelete(id);
+        return response.status(204).json(deleteCourtInformation);
+
+    } catch (error) {
+        console.log(error);
+        return response.status(500).json({msg: "Ops... algo de errado não está certo"});
+    }
+})
 
 export default router;
